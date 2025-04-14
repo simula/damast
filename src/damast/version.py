76abb7f4fe2f3dbe4bddb04a079d82a97ba8365c
Thomas Roehr <roehr@simula.no>
2025-04-14 12:36:20 +0200

    Update patch level

--- src/damast/version.py
+++ src/damast/version.py
@@ -1,6 +1,6 @@
 DAMAST_MAJOR_VERSION = 0
 DAMAST_MINOR_VERSION = 1
-DAMAST_PATCH_LEVEL = 2
+DAMAST_PATCH_LEVEL = 3
 
 __version_info__ = (str(DAMAST_MAJOR_VERSION), str(DAMAST_MINOR_VERSION), str(DAMAST_PATCH_LEVEL))
 __version__ = '.'.join(__version_info__)
