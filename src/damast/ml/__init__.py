7fb3d39cd16c5276b0af4a060318e4b9b0deb140
Thomas Roehr <roehr@simula.no>
2026-02-24 23:28:00 +0100

    ml: update backend autodiscovery

--- src/damast/ml/__init__.py
+++ src/damast/ml/__init__.py
@@ -7,12 +7,6 @@ logger = logging.getLogger(__name__)
 
 # To ensure that autoloading works as expected
 # from damast.ml import keras
-
-if "KERAS_BACKEND" in os.environ:
-    backend = os.environ['KERAS_BACKEND']
-    if not backend_available(backend):
-        raise RuntimeError("Keras backend: {backend} is not available")
-
 def backend_available(backend: str) -> bool:
     try:
         importlib.import_module(backend)
@@ -29,6 +23,14 @@ def autodiscover_backend(priority: list[str] = ["torch", "tensorflow", "jax"]):
         else:
             logger.info(f"Ignoring backend: {backend}, library is not available")
 
-autodiscover_backend()
+if "KERAS_BACKEND" in os.environ:
+    backend = os.environ['KERAS_BACKEND']
+    if not backend_available(backend):
+        raise RuntimeError(
+                "Keras backend: select backend '{backend}' is not available. "
+                "Please install the required package(s)."
+        )
+else:
+    autodiscover_backend()
 
 import keras
